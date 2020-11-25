c5cf5116124bed276a6d873e88d4b01c64a36b9c
Blast <naman07rm@gmail.com>
2020-11-25 20:52:47 +0530

    submission for problem 1557

---
+++ minimum-number-of-vertces-to-reach-all-nodes.cpp
@@ -0,0 +1,18 @@
+//1557
+
+vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+	vector <int> in(n, 0);    
+
+	for (auto edge:edges){
+		in[edge[1]]++;
+	}
+
+	vector <int> res;
+
+	for (int i=0;i<n;i++){
+		if (in[i] == 0)
+			res.push_back();
+	}
+
+	return res;
+}
